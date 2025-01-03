0f9b4bf638d1f8256e1f63d80d5c82331f7ef77e
venuchilukala <venuchilukala07@gmail.com>
2025-01-03 17:19:34 +0530

    Add to cart

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,8 +4,17 @@ import { RouterProvider } from "react-router-dom";
 import router from "./router/Router.jsx";
 import AuthProvider from "./contexts/AuthProvider.jsx";
 
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")).render(
   <AuthProvider>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </AuthProvider>
 );
--- client/src/pages/shop/CartPage.jsx
+++ client/src/pages/shop/CartPage.jsx
@@ -1,6 +1,109 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { FaTrash } from "react-icons/fa";
+import useCart from "../../hooks/useCart";
+import Swal from "sweetalert2";
+import { AuthContext } from "../../contexts/AuthProvider";
 
 const CartPage = () => {
+  const [cart, refetch] = useCart();
+  const { user } = useContext(AuthContext);
+  const [cartItems, setCartItems] = useState([]);
+
+  // Calculate Price
+  const calculatePrice = (item) => {
+    // return item.price * item.quantity
+  };
+
+  // Calculate total price
+  const calculateSubTotal = cartItems.reduce((total, item)=>{
+    // return total + calculatePrice(item)
+  },0)
+
+  // handleIncrease
+  const handleIncrease = (item) => {
+    fetch(`http://localhost:6001/carts/${"45521"}`, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+      body: JSON.stringify({ quantity: item.quantity + 1 }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        const updatedCart = cartItems.map((cartItem) => {
+          if (cartItem.id === item.id) {
+            return {
+              ...cartItem,
+              quantity: cartItem.quantity + 1,
+            };
+          }
+          return cartItem;
+        });
+        refetch();
+        setCartItems(updatedCart);
+      });
+    refetch();
+  };
+
+  // handleDecrease
+  const handleDecrease = (item) => {
+    if (item.quantity > 1) {
+      fetch(`http://localhost:6001/carts/${"45521"}`, {
+        method: "PUT",
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+        body: JSON.stringify({ quantity: item.quantity - 1 }),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          const updatedCart = cartItems.map((cartItem) => {
+            if (cartItem.id === item.id) {
+              return {
+                ...cartItem,
+                quantity: cartItem.quantity - 1,
+              };
+            }
+            return cartItem;
+          });
+          refetch();
+          setCartItems(updatedCart);
+        });
+      refetch();
+    } else {
+      alert("Item can't be lessthan zero");
+    }
+  };
+
+  const handleDelete = (item) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:6001/${"jskdj"}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount > 0) {
+              refetch();
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your file has been deleted.",
+                icon: "success",
+              });
+            }
+          });
+      }
+    });
+  };
+
   return (
     <div className="section-container">
       <div className="min-h-screen  xl:px-24 bg-gradient-to-r from-0% from-[#FAFAFA] to-[#FCFCFC] to-100%">
@@ -29,9 +132,7 @@ const CartPage = () => {
             <tbody>
               {/* row 1 */}
               <tr>
-                <td>
-                    1
-                </td>
+                <td>1</td>
                 <td>
                   <div className="flex items-center gap-3">
                     <div className="avatar">
@@ -42,27 +143,60 @@ const CartPage = () => {
                         />
                       </div>
                     </div>
-                    <div>
-                      <div className="font-bold">Hart Hagerty</div>
-                      <div className="text-sm opacity-50">United States</div>
-                    </div>
                   </div>
                 </td>
+                <td>Name of product</td>
+                <td>$0{calculatePrice("item").toFixed(2)}</td>
                 <td>
-                  Zemlak, Daniel and Leannon
-                  <br />
-                  <span className="badge badge-ghost badge-sm">
-                    Desktop Support Technician
-                  </span>
+                  <button
+                    className="btn btn-xs"
+                    onClick={() => handleDecrease("item")}
+                  >
+                    -
+                  </button>
+                  <input
+                    type="number"
+                    value="0"
+                    onChange={() => console.log("object")}
+                    className="w-10 mx-2 text-center overflow-hidden appearance-none"
+                  />
+                  <button
+                    className="btn btn-xs"
+                    onClick={() => handleIncrease("item")}
+                  >
+                    +
+                  </button>
                 </td>
-                <td>Purple</td>
                 <th>
-                  <button className="btn btn-ghost btn-xs">details</button>
+                  <button
+                    className="btn btn-ghost btn-sm text-red-500"
+                    onClick={() => handleDelete("item")}
+                  >
+                    <FaTrash />
+                  </button>
                 </th>
               </tr>
             </tbody>
             {/* foot */}
           </table>
+        </div>
+
+        {/* customer details */}
+        <div className="my-12 flex flex-col md:flex-row justify-between items-start">
+          <div className="md:w-1/2 space-y-3">
+            <h3 className="font-medium">Customer Details</h3>
+            <p>Name : {user.displayName}</p>
+            <p>Email : {user.email}</p>
+            <p>User id : {user.uid}</p>
+          </div>
+          <div>
+            <h3 className="font-medium">Shopping Details</h3>
+            <p>Total Items : {cart.length}</p>
+            <p>Total Price : 200.00</p>
+            <button className="btn btn-md btn-primary text-white">
+              Proceed Checkout
+            </button>
+          </div>
         </div>
       </div>
     </div>
