471312fa977e3e93f2ee870d89f57aa4413a57d8
venuchilukala <venuchilukala07@gmail.com>
2025-01-07 22:37:34 +0530

    store creation and updation

--- client/src/pages/dashboard/admin/UpdateProduct.jsx
+++ client/src/pages/dashboard/admin/UpdateProduct.jsx
@@ -63,7 +63,7 @@ const UpdateProduct = () => {
   return (
     <div className="w-full md:w-[870px] px-4 mx-auto">
       <h2 className="text-2xl font-semibold my-4">
-        Upload a New <span className="text-blue-500">Product</span>
+        Update a  <span className="text-blue-500">Product</span>
       </h2>
 
       {/* Form  */}
